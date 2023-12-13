0ba814584cd693cfb1b6d44dc43757a0c0f47a3a
Louise Macfadyen <louisejmacfadyen@gmail.com>
2023-12-13 00:10:10 -0500

    shapesssss

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Matter.js module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
@@ -6,7 +5,6 @@ var Engine = Matter.Engine,
     Mouse = Matter.Mouse,
     MouseConstraint = Matter.MouseConstraint;
 
-    // Array of hex codes for wall colors
 var wallColors = [
     '#396250',
     '#5260DA',
@@ -17,31 +15,26 @@ var wallColors = [
     '#AA692F'
 ];
 
-// Function to get a random wall color
 function getRandomWallColor() {
     var index = Math.floor(Math.random() * wallColors.length);
     return wallColors[index]; 
 }
 
-// Set a random color for walls
 var wallColor = getRandomWallColor();
 var shapeColor = getRandomShapeColor();
 
-// Function to get a random color different from wallColor
 function getRandomShapeColor() {
     var color;
     do {
         var index = Math.floor(Math.random() * wallColors.length);
         color = wallColors[index];
-    } while (color === wallColor); // Repeat if the color is the same as the wall color
+    } while (color === wallColor); 
     return color;
 }
 
-// Create an engine
 var engine = Engine.create(),
     world = engine.world;
 
-// Create a renderer
 var render = Render.create({
     element: document.body,
     engine: engine,
@@ -55,22 +48,18 @@ var render = Render.create({
     }
 });
 
-// Define the square boundary dimensions
 var squareTopLeftX = 0;
 var squareTopLeftY = 0;
 var squareSize = 400;
 
-
-// Define a more varied set of convex potato shapes
 var potatoes = [
     Matter.Vertices.fromPath('40 0, 60 20, 70 50, 50 70, 30 60, 10 40, 0 20'),
     Matter.Vertices.fromPath('30 10, 50 0, 70 20, 60 40, 40 60, 20 50, 10 30'),
-    Matter.Vertices.fromPath('50 0, 70 10, 80 30, 70 60, 50 70, 20 60, 10 40, 0 30'),
-    // More convex shapes can be added here
+    Matter.Vertices.fromPath('10 0, 70 10, 80 30, 70 60, 50 70, 20 60, 10 40, 0 30'),
+    Matter.Vertices.fromPath('20 20, 40 10, 60 20, 70 40, 60 60, 40 70, 20 60, 10 40'),
+    Matter.Vertices.fromPath('30 10, 50 10, 70 30, 60 60, 50 70, 30 70, 10 50, 10 30'),
 ];
 
-
-// Create the square boundary (as static bodies)
 var ground = Bodies.rectangle(squareTopLeftX + squareSize / 2, squareTopLeftY + squareSize, squareSize, 100, {
     isStatic: true,
     render: {
@@ -95,14 +84,14 @@ var topWall = Bodies.rectangle(squareTopLeftX + squareSize / 2, squareTopLeftY, 
         fillStyle: wallColor
     }
 });
-// Create and position varied potato shapes within the square
+
 var shapes = [];
-var rows = 4; // Number of rows of shapes
-var columns = 4; // Number of columns of shapes
+var rows = 4; 
+var columns = 4; 
 for (var i = 0; i < rows; i++) {
     for (var j = 0; j < columns; j++) {
         var randomPotato = potatoes[Math.floor(Math.random() * potatoes.length)];
-        var scale = 0.5 + Math.random(); // Random scale between 0.5 and 1.5
+        var scale = 0.5 + Math.random(); 
         var scaledPotato = randomPotato.map(function(vertex) {
             return { x: vertex.x * scale, y: vertex.y * scale };
         });
@@ -119,13 +108,10 @@ for (var i = 0; i < rows; i++) {
     }
 }
 
-// Add all of the bodies to the world
 World.add(world, [ground, leftWall, rightWall, topWall, ...shapes]);
 
-// Create a mouse
 var mouse = Mouse.create(render.canvas);
 
-// Create mouse constraint
 var mouseConstraint = MouseConstraint.create(engine, {
     mouse: mouse,
     constraint: {
@@ -136,19 +122,12 @@ var mouseConstraint = MouseConstraint.create(engine, {
     }
 });
 
-// Add mouse constraint to the world
 World.add(world, mouseConstraint);
-
-// Ensure the mouse is relative to the canvas and not the entire page
 render.mouse = mouse;
 
-// Run the engine
 Engine.run(engine);
-
-// Run the renderer
 Render.run(render);
 
-// Function to export canvas as PNG
 function exportCanvas() {
     var canvas = document.getElementById('myCanvas');
     var url = canvas.toDataURL("image/png");
@@ -158,7 +137,5 @@ function exportCanvas() {
     link.click();
 }
 
-// Attach export function to button
 document.getElementById('exportButton').addEventListener('click', exportCanvas);
-
 
